ea0d7912030c28fa407b8e6b14bc9308fcf901b1
dhameeshafernando-glitch <dhameeshafernando@gmail.com>
2026-01-13 19:53:23 +0530

    change S

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 def main():
-    print("Hello! Welcome to my simple Python script.")
+    print("Hello! Welcome to my simple Python Script.")
 
 if __name__ == "__main__":
     main()
